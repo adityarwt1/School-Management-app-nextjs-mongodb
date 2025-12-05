9b590d9997223087eff867a110baf362c656c8c5
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-05 22:53:15 +0530

    rote teacher added

--- app/api/v1/(teacher)/registerTeacher/route.ts
+++ app/api/v1/(teacher)/registerTeacher/route.ts
@@ -6,12 +6,14 @@ import { NextRequest, NextResponse } from "next/server";
 import jwt from 'jsonwebtoken'
 import { cookies } from "next/headers";
 import { Role } from "@/types/role";
+import bcrypt from "bcryptjs";
+
 export async function POST(req: NextRequest):Promise<NextResponse<TeacherRegisterResponse>> {
     try {
-        const {bcCode,contactNumber,diseCode,fullName,password,profilePicture,schoolId}:TeacherInterface = await req.json();
+        const {bcCode,contactNumber,diseCode,fullName,password,profilePicture,schoolId , classTeacher}:TeacherInterface = await req.json();
 
-        if(!bcCode || !contactNumber || !diseCode || !fullName || !password || !profilePicture || !schoolId){
-            return NextResponse.json({error:"Field not provided properly!" , success:false})
+        if(!bcCode || !contactNumber || !diseCode || !fullName || !password  || !schoolId || !classTeacher){
+            return NextResponse.json({error:"Field not provided properly!" , success:false},{status:400})
         }
 
         // connect databse
@@ -27,8 +29,8 @@ export async function POST(req: NextRequest):Promise<NextResponse<TeacherRegiste
             return NextResponse.json({error:"Teacher already exist!", success:false},{status:409})
         }
 
-
-        const teacher  = await Teacher.create({bcCode, contactNumber, diseCode, fullName, password, profilePicture , schoolId})
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const teacher  = await Teacher.create({bcCode, contactNumber, diseCode, fullName, password:hashedPassword, profilePicture , schoolId , classTeacher})
 
         if(!teacher){
             return NextResponse.json({error:"Failed to register!" , success:false},{status:500})
@@ -42,9 +44,9 @@ export async function POST(req: NextRequest):Promise<NextResponse<TeacherRegiste
         const token = jwt.sign(tokenPayload , process.env.JWT_SECRET as string);
 
         (await cookies()).set("smaToken" , token)
-        return NextResponse.json({ success: true }, { status: 200 })
+        return NextResponse.json({ success: true , token}, { status: 200 })
     } catch (error) {
-        console.log(error)
+        console.log((error as Error).message)
         return NextResponse.json({ error: "Internal server issue." , success:false}, { status: 500 })
     }
 }
