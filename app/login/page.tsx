897592de5bccc75de72a0762e09b268e5bd92e52
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-05 21:26:21 +0530

    login reset

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { loginStudent } from '@/services/Student/Login/loginStudent'
 import React, { ChangeEvent, useState } from 'react'
 
 const LoginPage = () => {
@@ -10,10 +9,7 @@ const LoginPage = () => {
     setRole(role)
   }
   console.log(role)
-  const loginStude = async()=>{
-    await loginStudent({ssmId: 141979665,password: "shivendra@123456789"})
-  }
-  loginStude()
+ 
   return (
     <div className='w-full h-screen justify-center items-center'>
       <div>
