4f998737d6f2397d4c653205f26b0e2b2657262f
Shivedra123 <shivendrasaket2580@gmail.com>
2025-12-05 21:43:44 +0530

    vishkarma

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+
+"use client"
+import React, {  ChangeEvent,useState } from 'react'
+
+
+
 
 const LoginPage = () => {
+
+  const [role, setrole] = useState('')
+
+const handler=(e)=>{
+
+  const role = e.target.value
+  setrole(role)
+
+}
+
+console.log(role)
+
+
+
+
 
   return (
     <div className='w-full h-screen justify-center items-center'>
       <div>
-        <select name="role">
+        <select name="role" onChange={handler}   >
           <option value="principle">Principle</option>
           <option value="teacher">Teacher</option>
           <option value="student">Student</option>
