1fd9b54b7ffcae6ee7bd663773e981b14eae3077
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-11 13:44:37 +0530

    services is ready for the principlre register

---
+++ services/Principle/Register.ts
@@ -0,0 +1,25 @@
+"use client"
+
+import { PrincipleRegisteredRequest, PrincipleRegisterResponse } from "@/interfaces/ApiResponse/Principle/registerInterface";
+
+export async function registerPrinciple(data:PrincipleRegisteredRequest):Promise<PrincipleRegisterResponse> {
+
+    try {
+        const response = await fetch("/api/v1/registerPrinciple",{
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(data)
+        })
+        const resData :PrincipleRegisterResponse = await response.json()
+        return resData
+    } catch (error) {
+        console.log(error)
+        return {
+            success:false,
+            error:"Internal server issue."
+        }
+    }
+    
+}
