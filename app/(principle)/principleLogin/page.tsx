d62f5990f6a0f6f90c3e137e0208e307d56b4503
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-18 13:27:48 +0530

    login of principle is done

--- app/(principle)/principleLogin/page.tsx
+++ app/(principle)/principleLogin/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { PrincipleLoginRequest } from '@/interfaces/ApiResponse/Principle/loginprinciple'
 import { PrincipleApi } from '@/services/Principle/Principle'
 import { useRouter } from 'next/navigation'
@@ -9,16 +10,29 @@ const PrincipleLoginPage = () => {
     bcCode:"",
     email:""
   })
+  console.log(data)
   const router = useRouter()
   const [isLoadin, setIsloading] = useState<boolean>(false)
   const [error, setError] = useState<string>('')
   const principleServices  = new PrincipleApi()
-  const handleChange  = (e:ChangeEvent<HTMLInputElement>)=>{
-    const {name, value} = e.target  
+
+  const handleFinalMove = (emailorBccode:string):string=>{
+    if(emailorBccode.includes("@")){
+      setData(prev =>({
+        ...prev,
+        "bcCode":""
+      }))
+      return "email"
+    }else{
+      return "bcCode"
+    }
+  }
+  const handleChange  =  (e:ChangeEvent<HTMLInputElement>)=>{
+    const {name,value} = e.target  
 
     setData(prev=> ({
       ...prev,
-      [name]:value
+      [name === "password" ? name :handleFinalMove(value)]:value
     }))
   }
   const hanldeSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
@@ -35,14 +49,22 @@ const PrincipleLoginPage = () => {
           setError("Something went wrong.")
         }
       } catch (error) {
-        setError("Something went wrong.")
+        setError( (error as Error).message ||  "Something went wrong.")
       }finally{
         setIsloading(false)
       }
   }
   
   return (
-    <div>PrincipleLoginPage</div>
+    <div className='flex justify-center items-center w-full h-fit pt-10'>
+      <form onSubmit={hanldeSubmit}>
+        <label htmlFor="bcCode">Bc Code or Email</label>
+        <input type="text" onChange={handleChange} required name='bcCode'/>
+        <label htmlFor="password">Password</label>
+        <input type="text" onChange={handleChange} required name='password'/>
+        <button type='submit' disabled={isLoadin}>{isLoadin ? "Login...":"Login"}</button>
+      </form>
+    </div>
   )
 }
 
