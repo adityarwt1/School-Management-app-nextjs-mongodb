fc398941522515c6cc44a2b8eaf2b349f353a159
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-19 21:09:14 +0530

    page.tsx

--- app/(teacher)/teacherRegister/page.tsx
+++ app/(teacher)/teacherRegister/page.tsx
@@ -6,17 +6,19 @@ import React, { useState } from 'react'
 
 const TeacherRegisterPage = () => {
   const [data, setData] = useState<TeacherRegisterInterface>({
-    bcCode:"",
-    contactNumber:0,
     diseCode:0,
+    bcCode:"",
     email:"",
     fullName:"",
+    contactNumber:0,
     password:""
   })
+  console.log(data)
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [error, setError] = useState<string>("")
   const router = useRouter()
-
+  const [showPassword, setSchowPassword] = useState<boolean>(false)
+  const [numField, setNumField] = useState(['diseCode','contactNumber'])
   const teacherApiServices = new TeacherApi()
   
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
@@ -24,7 +26,7 @@ const TeacherRegisterPage = () => {
 
     setData(prev=> ({
       ...prev,
-      [name]:value
+      [name]:numField.includes(name) ? Number(value):value
     }))
   }
 
@@ -50,16 +52,34 @@ const TeacherRegisterPage = () => {
       setIsLoading(false)
     }
   }
+
+  const handleShowPassword = ()=>{
+    setSchowPassword(!showPassword)
+  }
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label htmlFor="diseCode">Dise Code</label>
-        <input type="number" name='diseCode' onChange={handleChange}/>
+        <input type="number" name="diseCode" onChange={handleChange} />
         <label htmlFor="bcCode">BcCode</label>
-        <input type="text" onChange={handleChange} name='' />
+        <input type="text" onChange={handleChange} name="bcCode" />
+        <label htmlFor="email">Email</label>
+        <input type="text" onChange={handleChange} name="email" />
+        <label htmlFor="fullName">Full Name</label>
+        <input type="text" name="fullName" onChange={handleChange} />
+        <label htmlFor="contactNumber">ContactNumber</label>
+        <input type="text" name="contactNumber" onChange={handleChange} />
+        <label htmlFor="password">Password</label>
+        <div>
+          <input type="text" name="password" onChange={handleChange} />
+          <button onClick={handleShowPassword}>{!showPassword ?"Show":"Hide"}</button>
+        </div>
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Register..." : "Register"}
+        </button>
       </form>
     </>
-  )
+  );
 }
 
 export default TeacherRegisterPage
