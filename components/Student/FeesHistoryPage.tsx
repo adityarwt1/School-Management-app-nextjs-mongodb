321ca34ed3a41b7d484fd7b2c35f79c9d318dce3
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-21 19:19:03 +0530

    payment history card looking good

--- components/Student/FeesHistoryPage.tsx
+++ components/Student/FeesHistoryPage.tsx
@@ -2,12 +2,13 @@
 import { Months } from '@/enums/MonthEnut'
 import { PaymentHistory } from '@/interfaces/PayMent/Payment'
 import { PaymentServices } from '@/services/Payment/Payment'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 const FeesHistoryComponent = () => {
 
-  const [isLoding, setIsLoaind] = useState<boolean>(true)
+  const [isLoding, setIsLoaind] = useState<boolean>(false)
   const [feesData , setFeesData] = useState<PaymentHistory[]>([])
   const [error, setError] = useState<string>()
   const [refresh,setRefresh] = useState<number>(0)
@@ -15,6 +16,7 @@ const FeesHistoryComponent = () => {
 
   useEffect(()=>{
     const fetchData = async ()=>{
+      setIsLoaind(true)
       try {
         const paymentServices = new PaymentServices()
         const response = await paymentServices.getPaymentHistory()
@@ -31,23 +33,57 @@ const FeesHistoryComponent = () => {
       } catch (error) {
         console.log(error)
         setError("Failed to fetch Payment history")
+      } finally {
+       setIsLoaind(false)
       }
     }
 
     fetchData()
   },[refresh,router ])
 
+  if(isLoding){
+    return (
+      <div className='w-[30%] flex justify-between items-center px-2 py-5 '>
+        <div>Loading...</div>
+      </div>
+    )
+  }
+
   return (
-    <div className='bg-green-500 w-[30%] px-2 py-1'>
-      {feesData ? feesData.map((ele)=>(
-        <div key={ele._id as string} className='w-full px-4 py-1'>
-          <div>{Months[ele.month]}</div>
-        </div>
-      )):(
+    <div className="border mx-2 border-black/15 rounded-xl w-[30%] px-2  gap-2 flex flex-col py-5">
+      <div className="w-full px-4 py-1 flex justify-evenly">
+        <div className="w-full ">Month</div>
+        <div className="w-full ">₹Amount</div>
+        <div className="w-full">Remaining</div>
+        <div className="w-full">Pay</div>
+      </div>
+      {feesData ? (
+        feesData.map((ele) => (
+          <>
+            <div
+              key={ele._id as string}
+              className="w-full px-4 py-1 border border-black/20 rounded shadow flex justify-evenly"
+            >
+              <div className="text-left flex w-full">{Months[ele.month]}</div>
+              <div className="text-green-500 w-full">
+                <span className="font-mono">{`₹${ele.amount}`}</span>
+              </div>
+              <div className="text-yellow-600 w-full font-mono">{`₹${ele.remains}`}</div>
+              {ele.remains > 0 ?  (
+                <Link href={`/payFees?mode=payRemains&_id=${ele._id}&amount=${ele.remains}&month=${ele.month}`}>
+                  <button className='px-2 py-1 bg-yellow-600 text-nowrap text-white rounded-md cursor-pointer hover:opacity-95 w-full'>Pay {ele.remains}</button>
+                </Link>
+              ):(
+                <button className='bg-green-600 text-white px-2 py-1 rounded-md  hover:opacity-95 ' aria-disabled={true}>Paid</button>
+              )}
+            </div>
+          </>
+        ))
+      ) : (
         <div>Payment History not found</div>
       )}
     </div>
-  )
+  );
 }
 
 export default FeesHistoryComponent
